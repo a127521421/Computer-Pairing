9bea41cd0dee056453091bdb46e57536971526fe
a127521421 <a127521421@yahoo.com.tw>
2020-07-16 11:03:51 +0800

    輪播圖修改(功能成功)

--- shop/src/views/Home.vue
+++ shop/src/views/Home.vue
@@ -9,26 +9,33 @@
       indicators
       background="#ababab"
       img-width="100%"
-      img-height="auto"
+      img-height="100vh"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
       <!-- 輪播圖 -->
-      <b-carousel-slide v-for="(carousel, idx) in carousels" :key="idx" :img-src="carousels.src">
+      <b-carousel-slide v-for="(carousel, idx) in carousels" :key="idx" :img-src="carousel.src">
       </b-carousel-slide>
     </b-carousel>
 
   </div>
 </template>
-
+<style>
+  #carousel-1 img{
+    width:100%;
+    height:100vh;
+    object-fit: cover;
+    object-position: center;
+  }
+</style>
 <script>
 export default {
   data () {
     return {
       slide: 0,
       sliding: null,
-      carousel: []
+      carousels: []
     }
   },
   methods: {
@@ -44,7 +51,7 @@ export default {
       .then(response => {
         this.carousels = response.data.result.map(d => {
           return {
-            src: process.env.VUE_APP_APIURL + '/carousel/' + d.name
+            src: process.env.VUE_APP_APIURL + '/carousel/' + d.image
           }
         })
       })
