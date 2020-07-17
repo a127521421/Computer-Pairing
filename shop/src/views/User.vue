21d2ffec39f785fabd9541c0a0f1ab3bbe06911c
a127521421 <a127521421@yahoo.com.tw>
2020-07-17 16:35:32 +0800

    新增-使用者後台(未完成)

--- shop/src/views/User.vue
+++ shop/src/views/User.vue
@@ -1,5 +1,73 @@
 <template>
   <div class="user">
-    233
+    <b-container>
+      <b-row>
+        <b-col cols="12">
+          <h1 class="text-center">會員中心</h1>
+          <div class="container-bg">
+            <div class="mod-002 clearfix">
+              <h3 class="panel-title clearfix">
+                <b><i class="icon-paste-solid"></i>交易記錄</b>
+              </h3>
+            <div class="card-wrap item-a">
+              <div class="item">
+                <div class="card">
+                  <div class="info">
+                    <strong class="title"><a href="https://myaccount.books.com.tw/myaccount/myaccount/getOrder/?loc=web_order&amp;id=a92fabdac111e77172d0d988f12fdb10">訂單查詢</a></strong>
+                  </div>
+                <div class="note clearfix">
+                  <div class="row">
+                    <div class="cell">
+                      <ul>
+                        <li><strong>已出貨<b>0</b></strong></li>
+                        <li>已到門市 (僅國內超商取貨)：0</li>
+                        <li>宅配在途 (僅國內配送到府)：0</li>
+                      </ul>
+                    </div>
+                    <div class="cell">
+                      <ul>
+                        <li><strong>未出貨<b>0</b></strong></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!--mod-002 end-->
+
+                        <!--mod-002-->
+                        <div class="mod-002 clearfix">
+                            <h3 class="panel-title clearfix">
+                                <b><i class="icon-user-edit-solid"></i>修改密碼</b>
+                            </h3>
+                            <div class="card-wrap" id="member_wrap">
+                                <div class="item">
+                                    <div class="card">
+                                        <div class="info">
+                                            <strong class="title"><a href="https://myaccount.books.com.tw/myaccount/myaccount/changePassword/?loc=change_password&amp;id=a19bcd44c0687bf67f98ec2e8b3411c8">修改密碼</a></strong>
+                                        </div>
+                                        <ul class="note">
+                                            <li>修改您的會員登入密碼</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                </div>
+                                <div class="item">
+                                </div>
+                            </div>
+                        </div><!--mod-002 end-->
+
+                    </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<style>
+  h1{
+    font-family: '微軟正黑體';
+  }
+</style>
