ea667398e123e9857a92f61c55f50344af40f0c7
a127521421 <a127521421@yahoo.com.tw>
2020-07-31 16:38:35 +0800

    新增:願望清單(功能成功)

--- shop/src/views/Goods.vue
+++ shop/src/views/Goods.vue
@@ -10,8 +10,8 @@
             <h3>{{goods.name}}</h3>
             <br>
             <h4 style="color:red">NT.{{goods.price}}</h4>
-            <b-button id="button" variant="danger" v-if="click" @click=" click = !click">移除願望清單</b-button>
-            <b-button id="button" variant="light" v-else @click=" click = !click">加到願望清單</b-button>
+            <b-button id="button" variant="danger" v-if="click" @click="dele()">移除願望清單</b-button>
+            <b-button id="button" variant="light" v-else @click="add()">加到願望清單</b-button>
             <br>
             <p id="text13px">螢幕 : {{goods.Screen}}</p>
             <p id="text13px">作業系統 : {{goods.WorkingSystem}}</p>
@@ -30,6 +30,7 @@
 export default {
   data () {
     return {
+      wishlist: '',
       goods: {},
       click: false
     }
@@ -37,15 +38,66 @@ export default {
   computed: {
     id () {
       return this.$route.params.id
+    },
+    user () {
+      return this.$store.getters.user
     }
   },
   methods: {
+    add () {
+      this.axios.post(process.env.VUE_APP_APIURL + '/wishlist',
+        { wishlist: this.goods._id }
+      )
+        .then(response => {
+          const data = response.data
+          if (data.success) {
+            this.click = true
+            this.wishlist = data._id
+          } else {
+            alert(data.message)
+          }
+        })
+        .catch(error => {
+          // 如果回來的狀態不是 200，顯示回來的 message
+          alert(error.response.data.message)
+        })
+    },
+    dele () {
+      this.axios.delete(process.env.VUE_APP_APIURL + '/wishlist/' + this.wishlist)
+        .then(response => {
+          const data = response.data
+          if (data.success) {
+            this.click = false
+            this.wishlist = ''
+          } else {
+            alert(data.message)
+          }
+        })
+        .catch(() => {
+          alert('發生錯誤')
+        })
+    }
   },
   mounted () {
     this.axios.get(process.env.VUE_APP_APIURL + '/good/' + this.$route.params.id)
       .then(response => {
         response.data.result.image = process.env.VUE_APP_APIURL + '/commodity/' + response.data.result.image
         this.goods = response.data.result
+        this.axios.post(process.env.VUE_APP_APIURL + '/wishlist/' + this.user.account, { wishlist: this.goods._id })
+          .then(response => {
+            const data = response.data
+            if (data.success === true) {
+              this.click = true
+              this.wishlist = data.result[0]._id
+            } else {
+              this.click = false
+              this.wishlist = ''
+            }
+          })
+          .catch(error => {
+          // 如果回來的狀態不是 200，顯示回來的 message
+            alert(error.response.data.message)
+          })
       })
       .catch((error) => {
         console.log(error)
