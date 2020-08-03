494001e2d1da38711cbd28219e0c7448e2e02b3c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-03 07:13:29 +0000

    Deploy a127521421/Computer-Pairing to github.com/a127521421/Computer-Pairing.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.0702b8c65a9fe654beb3426e372bad97.js"
+  "precache-manifest.b0b173281cc5ba1b34160bc998338444.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "shop"});
