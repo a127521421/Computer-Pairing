3337d6dcaa67dd85d3052427d159728aae20be76
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-03 01:24:15 +0000

    Deploy a127521421/Computer-Pairing to github.com/a127521421/Computer-Pairing.git:gh-pages

---
+++ precache-manifest.6ab8319edf4d3f979ebff038b6678ed6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c590979937fcb993671e",
+    "url": "/css/app.9a237293.css"
+  },
+  {
+    "revision": "c6654e95b331e4483366",
+    "url": "/css/chunk-vendors.084c0951.css"
+  },
+  {
+    "revision": "1946b5c132b5751d5c53bb1c80bff159",
+    "url": "/img/login.1946b5c1.gif"
+  },
+  {
+    "revision": "1946b5c132b5751d5c53bb1c80bff159",
+    "url": "/img/login.gif"
+  },
+  {
+    "revision": "f199d9d9828f714d07feb6463a2bd41f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a7a3375dcba2673695",
+    "url": "/js/administrator.f9092b9c.js"
+  },
+  {
+    "revision": "c590979937fcb993671e",
+    "url": "/js/app.8f2f734a.js"
+  },
+  {
+    "revision": "39d87d9cdb996763e023",
+    "url": "/js/changepassword.e39805f5.js"
+  },
+  {
+    "revision": "c6654e95b331e4483366",
+    "url": "/js/chunk-vendors.14f61773.js"
+  },
+  {
+    "revision": "9bb60344c0cbab0a9f15",
+    "url": "/js/goods.3cc0f472.js"
+  },
+  {
+    "revision": "87fae2edaf0df41e3b52",
+    "url": "/js/login.53eee55f.js"
+  },
+  {
+    "revision": "e6aad8703ee1366f8786",
+    "url": "/js/managecarousels.e0430a69.js"
+  },
+  {
+    "revision": "0b81b9b1c895b1570c81",
+    "url": "/js/managegoods.dd8d9619.js"
+  },
+  {
+    "revision": "187da76e84daf87cb071",
+    "url": "/js/reg.1b2d1ed1.js"
+  },
+  {
+    "revision": "ef88e1030363325fd3db",
+    "url": "/js/shopping.7c9da6dd.js"
+  },
+  {
+    "revision": "3ebc0711aeb8d6e5b157",
+    "url": "/js/user.3b59f799.js"
+  },
+  {
+    "revision": "7dbdabe7132fa722ace0ae954b1cc514",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
